8759051e9631156455f213ddabc4e1276f17266d
shirleykkky <19952130866@189.cn>
2026-09-17 10:17:19 +0800

    sw v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wardrobe-v3';
+const CACHE = 'wardrobe-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
